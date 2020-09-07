649ec235ab762c1e32ba99fad0c3044a15b62b54
ArgentLo <argent.lo.hk@gmail.com>
2020-09-07 21:42:13 +0900

    initial push

---
+++ config.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+
+# path to resume training
+RESUME_WEIGHT = "/home/argent/kaggle/Cornell_BirdSong_Reg/fold0/checkpoints/train.55_full.pth"
+
+INIT_LR = 8e-4
+
+
+model_config = {
+    "sample_rate": 32000,
+    "window_size": 1024,
+    "hop_size": 320,
+    "mel_bins": 64,
+    "fmin": 50,
+    "fmax": 14000,
+    "classes_num": 264  # 527 in train
+}
+
+SR = 32000  # sampling rate
+
+ROOT = "/home/argent/kaggle/Cornell_BirdSong_Reg/"
+DATA_PATH = ROOT + "datasets/"
+
+# train
+RAW_TRAIN_AUDIO_PATH = DATA_PATH + "raw_train_audio/"
+RESAMPLED_TRAIN_AUDIO_PATH = DATA_PATH + "resampled_train_audio/"
+train_csv = pd.read_csv(RESAMPLED_TRAIN_AUDIO_PATH + "train_mod.csv")
+
+# test
+# TEST_AUDIO_DIR = DATA_PATH + "test_audio/"
+TEST_AUDIO_DIR = DATA_PATH + "test_birdcall_check/test_audio/"
+test_csv = pd.read_csv(DATA_PATH + "test_birdcall_check/test.csv")
---
+++ train.py
@@ -0,0 +1,185 @@
+import cv2
+import audioread
+import logging
+import os
+import random
+import time
+
+import librosa
+import librosa.display as display
+import numpy as np
+import pandas as pd
+import soundfile as sf
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data as data
+
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Optional, List
+
+from catalyst.dl import SupervisedRunner, State, CallbackOrder, Callback, CheckpointCallback
+from fastprogress import progress_bar
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import f1_score, average_precision_score
+
+
+import config as global_config
+from dataset import PANNsDataset, INV_BIRD_CODE
+from model import PANNsCNN14Att
+from model_layers import AttBlock, ConvBlock
+from utils import *
+import warnings
+warnings.simplefilter("ignore")
+
+class PANNsLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # self.bce = nn.BCELoss()
+        self.bce_logit = nn.BCEWithLogitsLoss()  # BCEWithLogitsLoss more stable
+
+    def forward(self, input, target):
+        input_ = input["clipwise_output"]  # logit
+        # input_ = torch.where(torch.isnan(input_), torch.zeros_like(input_), input_)
+        # input_ = torch.where(torch.isinf(input_), torch.zeros_like(input_), input_)
+
+        target = target.float()
+        return self.bce_logit(input_, target)
+
+
+def train_model():
+
+    tmp_list = []
+    all_birds_dirs = Path(global_config.RESAMPLED_TRAIN_AUDIO_PATH)
+
+    for ebird_d in all_birds_dirs.iterdir():
+        if ebird_d.is_file():
+            continue
+        for wav_f in ebird_d.iterdir():
+            tmp_list.append([ebird_d.name, wav_f.name, wav_f.as_posix()])
+            
+    print(f">>> Total training examples: {len(tmp_list)}\n\n", tmp_list[:3])
+
+
+    train_wav_path_exist = pd.DataFrame(
+        tmp_list, columns=["ebird_code", "resampled_filename", "file_path"])
+
+    train_all = pd.merge(
+        global_config.train_csv, train_wav_path_exist, on=["ebird_code", "resampled_filename"], how="inner")
+
+
+    ##############################################################
+    #######   K-Fold split on each bird kind (ebird_code)  #######
+    ##############################################################
+
+    skf = StratifiedKFold(n_splits=9, shuffle=True, random_state=42)
+
+    train_all["fold"] = -1
+    for fold_id, (train_index, val_index) in enumerate(skf.split(train_all, train_all["ebird_code"])):
+        # df["fold"] == fold_id
+        train_all.iloc[val_index, -1] = fold_id
+        
+    # check the propotion
+    fold_proportion = pd.pivot_table(train_all, index="ebird_code", columns="fold", values="xc_id", aggfunc=len)
+    print(f">>> Number of bird kinds: {fold_proportion.shape[0]} \n>>> Number of folds: {fold_proportion.shape[1]}")
+
+    val_fold_num = 4
+
+    train_file_list = train_all.query("fold != @val_fold_num")[["file_path", "ebird_code"]].values.tolist()
+    val_file_list   = train_all.query("fold == @val_fold_num")[["file_path", "ebird_code"]].values.tolist()
+
+    print(">>> Valid_Fold: [fold {}] train: {}, val: {}".format(val_fold_num, len(train_file_list), len(val_file_list)))
+
+
+
+    ##############################################
+    ###########       Model Setup      ###########
+    ##############################################
+
+    device = torch.device("cuda:0")
+
+    # loaders
+    loaders = {
+        "train": data.DataLoader(PANNsDataset(train_file_list, None), 
+                                batch_size=64, 
+                                shuffle=True, 
+                                num_workers=2, 
+                                pin_memory=True, 
+                                drop_last=True),
+        
+        "valid": data.DataLoader(PANNsDataset(val_file_list, None), 
+                                batch_size=64, 
+                                shuffle=False,
+                                num_workers=2,
+                                pin_memory=True,
+                                drop_last=False)
+    }
+
+
+    # model
+    global_config.model_config["classes_num"] = 527
+    model = PANNsCNN14Att(**global_config.model_config)
+    weights = torch.load("/home/argent/kaggle/Cornell_BirdSong_Reg/pretrained_models/Cnn14_DecisionLevelAtt_mAP_0.425.pth")
+
+    # Load Pretrained Weight
+    model.load_state_dict(weights["model"])
+    model.att_block = AttBlock(2048, 264, activation='linear')
+    model.att_block.init_weights()
+    model.to(device)
+    print(f">>> Pretrained Model is loaded to {device}!")
+
+    # Optimizer
+    optimizer = optim.Adam(model.parameters(), lr=global_config.INIT_LR)
+
+    # Scheduler
+    NUM_EPOCHS = 100
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=int(NUM_EPOCHS/10))  # 5 cycles
+
+    # Loss
+    criterion = PANNsLoss().to(device)
+
+    # Resume
+    if global_config.RESUME_WEIGHT:
+        # callbacks
+        callbacks = [
+            F1Callback(input_key="targets", output_key="logits", prefix="f1"),
+            mAPCallback(input_key="targets", output_key="logits", prefix="mAP"),
+            CheckpointCallback(save_n_best=5, resume=global_config.RESUME_WEIGHT)  # save 5 best models
+        ]
+    else:
+        # callbacks
+        callbacks = [
+            F1Callback(input_key="targets", output_key="logits", prefix="f1"),
+            mAPCallback(input_key="targets", output_key="logits", prefix="mAP"),
+            CheckpointCallback(save_n_best=5)  # save 5 best models
+        ]
+
+
+    # Model Training
+    runner = SupervisedRunner(
+        device=device,
+        input_key="waveform",
+        input_target_key="targets"
+    )
+
+    runner.train(
+        model=model,
+        criterion=criterion,
+        loaders=loaders,
+        optimizer=optimizer,
+        scheduler=scheduler,
+        num_epochs=NUM_EPOCHS,
+        verbose=True,
+        logdir=f"fold0",
+        callbacks=callbacks,
+        main_metric="epoch_f1",  # metric to select the best ckpt
+        minimize_metric=False
+    )
+
+
+if __name__ == '__main__':
+
+    set_seed(444)
+    train_model()
